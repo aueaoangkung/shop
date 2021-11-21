7cb9db812974221eeb0d4eae7774143cec503099
aueaoangkung <sroiaudom@gmail.com>
2021-11-22 06:01:48 +0700

    1

--- src/data.js
+++ src/data.js
@@ -2,20 +2,20 @@ const data = {
   products: [
     {
       id: '1',
-      name: 'Stawberry',
-      price: 350,
-      image:'https://picsum.photos/id/1080/6858/4574',
+      name: 'แตงโม',
+      price: 100,
+      imagesrc :'1.jpg',
     },
     {
       id: '2',
-      name: 'Cactus',
-      price: 30,
-      image: 'https://picsum.photos/id/248/3872/2592',
+      name: 'ส้ม',
+      price: 50,
+      imagesrc :'2.jpg',
     },
     {
-      id: '3',
-      price: 500,
-      image: 'https://picsum.photos/id/102/4320/3240',
+      id: 'มะพร้าว',
+      price: 100,
+      imagesrc :'3.jpg',
     },
   ],
 };
