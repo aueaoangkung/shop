610966238126cca545569bd1c97de6b1bb0c9a66
aueaoangkung <sroiaudom@gmail.com>
2021-11-22 05:40:06 +0700

    540

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Stawberry',
+      price: 350,
+      image:'https://picsum.photos/id/1080/6858/4574',
+    },
+    {
+      id: '2',
+      name: 'Cactus',
+      price: 30,
+      image: 'https://picsum.photos/id/248/3872/2592',
+    },
+    {
+      id: '3',
+<<<<<<< HEAD
+      name: 'Raspberry',
+=======
+      name: 'Raspbarry',
+>>>>>>> 1cf753274b79cd8eb0fc50e5daa8643c133d7d40
+      price: 500,
+      image: 'https://picsum.photos/id/102/4320/3240',
+    },
+  ],
+};
+export default data;
